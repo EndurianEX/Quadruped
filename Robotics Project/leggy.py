8bf4770ca09c65d8cbd46386779b94c4882b6698
EndurianEX <140574231+EndurianEX@users.noreply.github.com>
2024-11-22 11:51:09 +0800

    Update leggy.py
    Add some documentation to the leggy.py script

--- Robotics Project/leggy.py
+++ Robotics Project/leggy.py
@@ -2,6 +2,8 @@ from motor import Servo
 import time
 import sys
 
+
+# setting the servo pins, this may need an update based on robot used
 frontRightFoot = Servo(pin=0)
 frontRightHip = Servo(pin=1)
 frontLeftFoot = Servo(pin=8)
@@ -11,8 +13,22 @@ backRightHip = Servo(pin=5)
 backLeftFoot = Servo(pin=12)
 backLeftHip = Servo(pin=13)
 
+# reading sent data from the Quadruped script
 while True:
-    v = sys.stdin.readline().split(",")
+    """
+    splits read string into 8 values between 0 and 7 and applies them to 8 abreviated angles for each limb
+    F = front
+    B = back
+    R = right
+    L = left
+    H = hip
+    F = foot
+
+    EXAMPLE:
+    FRH = frontRightHip
+    """
+    
+    v = sys.stdin.readline().split(",") 
     FRH_currentAngle = int(v[0])
     FRF_currentAngle = int(v[1])
     FLH_currentAngle = int(v[2])
@@ -22,7 +38,7 @@ while True:
     BLH_currentAngle = int(v[6])
     BLF_currentAngle = int(v[7])
             
-            
+    # Apply angles to the move() function from the motor library
     frontRightHip.move(FRH_currentAngle)
     frontRightFoot.move(FRF_currentAngle)
     frontLeftHip.move(FLH_currentAngle)
